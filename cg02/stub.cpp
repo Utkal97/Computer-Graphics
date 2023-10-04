056aa2f1910143402fc1c6f0b9e19d0096c5ffbb
Utkal97 <utkal.s15@iiits.in>
2023-10-03 20:38:57 -0400

    draw circle

--- cg02/stub.cpp
+++ cg02/stub.cpp
@@ -59,14 +59,14 @@ void drawCubicBezier(int* ptX, int* ptY) {
 // Main tasks for 6050
 /***************************************************************************/
 
-struct center {
+struct point {
 	int X, Y;
 }
 
-struct center findCenter(int x1, int y1, int x2, int y2, int x3, int y3) {
+struct point findCenter(int x1, int y1, int x2, int y2, int x3, int y3) {
 
-	struct center C;
-	float midPtX12 = (x1 + x2) / 2, midPtY12 = (y1 + y2) / 2,	//Mid Point of 1st chord: pt1-pt2
+	struct point C;
+	float midPtX12 = (x1 + x2) / 2, midPtY12 = (y1 + y2) / 2,	//Mid point of 1st chord: pt1-pt2
 		slope12 = (x1!=x2) ? (y2 - y1) / (x2 - x1) : 0;						//Slope of 1st chord
 	
 	float midPtX23 = (x2 + x3) / 2, midPtY23 = (y2 + y3) / 2,	//Mid point of 2nd chord: pt2-pt3
@@ -84,19 +84,137 @@ struct center findCenter(int x1, int y1, int x2, int y2, int x3, int y3) {
 		return;
 	}
 
-	C.X = ((b1 * c2) - (b2 * c1)) / ((a1 * b2) - (a2 * b1));
-	C.Y = ((c1 * a2) - (c2 * a1)) / ((a1 * b2) - (a2 * b1));
+	C.X = (int)(((b1 * c2) - (b2 * c1)) / ((a1 * b2) - (a2 * b1)));
+	C.Y = (int)(((c1 * a2) - (c2 * a1)) / ((a1 * b2) - (a2 * b1)));
 
 	return C;
 }
 
-void drawArc(int ptX1, int ptY1, int ptX2, int ptY2, int ptX3, int ptY3) {
-	drawPixel(ptX1, ptY1);
-	drawPixel(ptX2, ptY2);
-	drawPixel(ptX3, ptY3);
+/*
+	Checks whether given point "currentPt" lies on the arc from pt1 to pt2, where testPt is a point on arc
+*/
+boolean pointLiesOnArc(struct pt1, struct pt2, struct comparePt, struct currentPt) {
 	
-	struct center C = findCenter( ptX1, ptY1, ptX2, ptY2, ptX3,  ptY3);
-	printf("Center: %d %d", C.X, C.Y);
+	//If X1, X2 are same, slope will be infinite, here we need to check X cordinates
+	if(pt1.X == pt2.X) {
+		return ((comparePt.X - pt1.X) * (currentPt.X - pt1.X)) >= 0;
+	}
+
+	//Equation of line passing through points pt1, pt2 is given by
+	//y = (slope)*x + (y2 - (slope*x2))
+	float slope = (pt2.Y - pt1.Y) / (pt2.X - pt1.X);	//If X1==X2, slope value is infinity
+	float yIntercept = pt2.Y - (slope*pt2.X);
+
+	//If the same line is written as ax + by + c = 0, then
+	//a = -1*slope13, b = 1, c= -1*yIntercept
+	float a = -1*slope, b = 1, c = -1*yIntercept;
+
+	//A point on the circle lies on the arc if it lies on the same side as that of any other point on arc
+	//We have a third point (ptX2, ptY2) for testing
+	float comparePointWithLineEquation = a*comparePt.X + b*comparePt.Y + c,
+		currentPointWithLineEquation = a*currentPt.X + b*currentPt.Y + c;
+
+	if(comparePointWithLineEquation == 0) {
+		printf("Cannot draw circle, since the third point is lieing on the chord\n");
+		return false;
+	} else if(comparePointWithLineEquation * currentPointWithLineEquation > 0) {	//If both are on same side
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void drawArc(int ptX1, int ptY1, int ptX2, int ptY2, int ptX3, int ptY3) {
+	
+	struct point C = findCenter( ptX1, ptY1, ptX2, ptY2, ptX3,  ptY3);
+	printf("Center of circle: %d %d", C.X, C.Y);
+
+	struct pt1, pt2, testPt, currentPt;
+	pt1.X = ptX1;
+	pt1.Y = ptY1;
+	pt2.X = ptX2;
+	pt2.Y = ptY2;
+	testPt.X = ptX3;
+	testPt.Y = ptY3;
+
+	const radius = (int)(sqrt( pow((pt1.X-C.X),2) + pow((pt1.Y - C.Y),2) ));
+	
+	currentPt.X = 0;
+	currentPt.Y = radius;
+
+	if(pointLiesOnArc(pt1, pt2, testPt, currentPt)) {
+		drawPixel(currentPt.X, currentPt.Y);
+	}
+
+	float decisionParameter	= 5/4 - radius;
+
+	while(currentPt.X <= currentPt.Y) {
+		
+		currentPt.X += 1;
+		
+		if(decisionParameter < 0) {
+			decisionParameter += 2*(currentPt.X) + 1;
+		} else {
+			currentPt.Y -= 1;
+			decisionParameter += 2*(currentPt.X) + 1 - 2*(currentPt.Y);
+		}
+
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt)) {
+			drawPixel(currentPt.X, currentPt.Y);
+		}
+		struct currentPt2;
+		struct currentPt3;
+		struct currentPt4;
+		struct currentPt5;
+		struct currentPt6;
+		struct currentPt7;
+		struct currentPt8;
+
+		currentPt2.X = currentPt.Y;
+		currentPt2.Y = currentPt.X;
+
+		currentPt3.X = -1*currentPt.Y;
+		currentPt3.Y = currentPt.X;
+
+		currentPt4.X = -1*currentPt.X;
+		currentPt4.Y = currentPt.Y;
+
+		currentPt5.X = -1*currentPt.X;
+		currentPt5.Y = -1*currentPt.Y;
+
+		currentPt6.X = -1*currentPt.Y;
+		currentPt6.Y = -1*currentPt.X;
+
+		currentPt7.X = currentPt.Y;
+		currentPt7.Y = -1*currentPt.X;
+
+		currentPt8.X = currentPt.X;
+		currentPt8.Y = -1*currentPt.y;
+
+
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt2)) {
+			drawPixel(currentPt2.X, currentPt2.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt3)) {
+			drawPixel(currentPt3.X, currentPt3.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt4)) {
+			drawPixel(currentPt4.X, currentPt4.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt5)) {
+			drawPixel(currentPt5.X, currentPt5.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt6)) {
+			drawPixel(currentPt6.X, currentPt6.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt7)) {
+			drawPixel(currentPt7.X, currentPt7.Y);
+		}
+		if(pointLiesOnArc(pt1, pt2, testPt, currentPt8)) {
+			drawPixel(currentPt8.X, currentPt8.Y);
+		}
+	}
+
 	return;
 }
 
